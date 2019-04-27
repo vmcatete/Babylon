ef48d22aab41bda04f4cb11a8da17b227e4fbaf6
Veronica Catete <vmcatete@ncsu.edu>
2019-04-27 01:38:22 -0400

    updated skybox

--- game/js/createScene.js
+++ game/js/createScene.js
@@ -43,7 +43,16 @@ var createScene = function (baseInformation, updatedScores) {
     engine.loadingUIText = "Mario Party Board loading...";
     engine.displayLoadingUI();
 
-    generateTerrain(mainScene);
+    generateTerrain(mainScene);	
+	
+    var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("../assets/textures/skybox", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
     
     playerList = loadCharacters(mainScene);
     setTimeout(function() {    
